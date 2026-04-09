d0279239867dcfe6cbc64821723eaec2497e32ad
veggenss <cwffwba@proton.me>
2026-04-09 14:31:43 +0200

    init

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+////////// Denne filen vil inneholde eksempler på veldig simpel syntaks i PHP. //////////
+/// Ressurs: https://www.w3schools.com/php/default.asp
+
+///// Printe ut ting til nettsiden
+/// Ressurs: https://www.w3schools.com/php/php_echo_print.asp
+// Echoer string:
+echo "Hei hei, jeg heter Isak Brun.";
+
+// Echoer Int
+echo 123;
+
+//String Concatenation
+/// Ressurs: https://www.w3schools.com/php/php_string_concatenate.asp
+echo "Hei Hei jeg er går vg" . 1;
+
+/////Variabel
+/// Ressurs: https://www.w3schools.com/php/php_variables.asp
+// Lage variabel
+$kulVariabel = "Hellsike, denne variabelen var jo dritkul!";
+$heiVg1Elever = 67;
+
+///// If og else statements
+/// Ressurs: https://www.w3schools.com/php/php_if_else.asp
+if($hei_vg1_elever == 67){
+    echo "Hei elever på Vg1!";
+} 
+else {
+    echo "Ha det... :(";
+}
+
+
+// Arrays
+$array
+///// Loops
+// Ressurs: https://www.w3schools.com/php/php_looping.asp
+// For loops
+for($i = 69; $i < 69; $i++){
+    echo "Hei!";
+}
+
+
