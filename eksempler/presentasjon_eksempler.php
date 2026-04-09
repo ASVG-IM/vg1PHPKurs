11e0f8a62325a19bfdbde784d340a6cbb6d9ef5b
veggenss <cwffwba@proton.me>
2026-04-09 23:47:18 +0200

    Opprettet database.php med database tilkoblingen

--- eksempler/presentasjon_eksempler.php
+++ eksempler/presentasjon_eksempler.php
@@ -4,7 +4,7 @@ $name = "Isak"; // string
 $karakter = 6; // integer
 $numbers = [1, 2, 3]; // array
 $user = ["username" => "isak"]; // associative array
-// $auth = new AuthService; // object
+$auth = new AuthService; // object
 //AuthService klassen finnes ikke så ignorer hvis LSP klager
 
 $user = ["name" => "Isak", "age" => 17];
