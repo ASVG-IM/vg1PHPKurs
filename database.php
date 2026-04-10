0949c4672710f2d95a74e7655e2ee451bc482e21
veggenss <cwffwba@proton.me>
2026-04-10 08:53:34 +0200

    fjernet database.php fra root

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new \mysqli(
-    "", //Database host, f.eks: 127.0.0.1 eller localhost
-    "", //Database brukernavn, brukernavnet du logger inn på phpmyadmin med
-    "", //Database passord, passordet du logger inn på phpmyadmin med
-    "", //Database navnet, navnet til databsen du vil koble til
-);
