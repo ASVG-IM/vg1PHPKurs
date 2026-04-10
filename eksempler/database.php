7923e342f88cb53b2ee91957eaa78687f6667e84
veggenss <cwffwba@proton.me>
2026-04-10 03:19:58 +0200

    SQL eksmpler og database tilkobling

---
+++ eksempler/database.php
@@ -0,0 +1,7 @@
+<?php
+$conn = new \mysqli(
+    "", //Database host, f.eks: 127.0.0.1 eller localhost
+    "", //Database bruker navn, brukernavnet du logger inn på phpmyadmin med
+    "", //Database bruker passord, passordet du logger inn på phpmyadmin med
+    "", //Database navnet, navnet til databsen du vil koble til
+);
--- eksempler/presentasjon_eksempler.php
+++ eksempler/presentasjon_eksempler.php
@@ -1,4 +1,9 @@
 <?php
+// register.php
+require __DIR__ . '/database.php'; 
+print_r($conn); //$conn hentet fra database.php
+// henter $conn variabelet som da har database tilkoblingen
+
 ///kort datatyper
 $name = "Isak"; // string
 $karakter = 6; // integer
@@ -11,3 +16,24 @@ $user = ["name" => "Isak", "age" => 17];
 
 echo $user["name"];
 // OUTPUT: Isak
+
+//stmts som trenger parametre
+$stmt = $conn->prepare('SELECT * FROM users WHERE id = ?;'); 
+$stmt->bind_param("i", $userId); // plasserer ? verdier, $userId er integer derfor "i"
+$stmt->execute(); // kjører stmt, returnerer true eller false
+$result = $stmt->get_result(); // $result har nå stmt resultatet
+$data = $result->fetch_assoc(); // henter mysqli_result som et associative array
+
+//stmts uten parametre
+$query = $conn->query('SELECT * FROM users;'); // $query har mysqli resultatet
+$qData = $query->fetch_all(MYSQLI_ASSOC); // henter mysqli_result som et associative array
+
+//insert statement
+$insert = $conn->prepare('INSERT INTO users (username, password) VALUES (?, ?);'); // forbereder statement
+$insert->bind_param("ss", $username, $password); // både $username og $password er strings derfor "ss"
+if(!$stmt->execute()){ // if løkker kjører koden inni seg, så vi kan kjøre execute og se om den failer på en kompakt måte
+    return false;
+}
+else{
+    return true;
+}
